7ddefd0291ab92703aa878a8061aba63606633f2
libdefi <joji.sekiya@gmail.com>
2023-06-04 16:26:26 +0900

    fixed some bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,24 @@
+
 'use client'
-import Image from 'next/image'
 import {
   useState
 } from 'react'
 
 export default function Home() {
-  const [query, setQuery]= useState('')
-  const [result, setResult]= useState('')
-  const [loading, setLoading]= useState(false)
-
-  async function createIndexAndEmbeddings(){
+  const [query, setQuery] = useState('')
+  const [result, setResult] = useState('')
+  const [loading, setLoading] = useState(false)
+  async function createIndexAndEmbeddings() {
     try {
       const result = await fetch('/api/setup', {
-        method: 'POST',
+        method: "POST"
       })
       const json = await result.json()
-      console.log('result:', json)
+      console.log('result: ', json)
     } catch (err) {
       console.log('err:', err)
     }
   }
-
   async function sendQuery() {
     if (!query) return
     setResult('')
@@ -28,9 +26,9 @@ export default function Home() {
     try {
       const result = await fetch('/api/read', {
         method: "POST",
-        body: JSON.stringify({query}),
+        body: JSON.stringify(query)
       })
-      const json =await result.json()
+      const json = await result.json()
       setResult(json.data)
       setLoading(false)
     } catch (err) {
@@ -38,24 +36,18 @@ export default function Home() {
       setLoading(false)
     }
   }
-  
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <input 
-        className='text-black px-2 py-1'
-        onChange={e => setQuery(e.target.value)}
-      />
-      <button className="px-7 py-1 rounded-2xl bg-white text-black mt-2 mb-2" onClick={sendQuery}>
-        Ask AI
-      </button>
+      <input className='text-black px-2 py-1' onChange={e => setQuery(e.target.value)} />
+      <button className="px-7 py-1 rounded-2xl bg-white text-black mt-2 mb-2" onClick={sendQuery}>Ask AI</button>
       {
-        loading && <p>Asking AI...</p>
+        loading && <p>Asking AI ...</p>
       }
       {
         result && <p>{result}</p>
       }
+      { /* consider removing this button from the UI once the embeddings are created ... */}
       <button onClick={createIndexAndEmbeddings}>Create index and embeddings</button>
     </main>
-    
   )
 }
